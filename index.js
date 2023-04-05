d048db3b9da2d127367684da31e2d562b24b6b0f
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-04-06 02:33:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -137,8 +137,8 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 5000
-app.listen(3000, function() {
-  console.log('Express app listening on port 3000');
+app.listen(8080, function() {
+  console.log('Express app listening on port 8080');
 });
 
 
