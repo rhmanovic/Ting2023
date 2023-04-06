44795897c76506777cb4619093ed4be9c2a86770
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-04-07 02:50:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,6 @@ app.set('views/manager', __dirname + '/views/manager');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log("404");
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
@@ -130,7 +129,6 @@ app.use(function(req, res, next) {
 // error handler
 // define as the last app.use callback
 app.use(function(err, req, res, next) {
-  console.log("yes 404");
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
