d9e55885272e7a5d84f08fd4973ec96467047e95
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-04-07 02:39:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.set('views/manager', __dirname + '/views/manager');
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(err, req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
