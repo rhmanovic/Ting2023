c62680a6be014aad1469dc8d2b9db9de30eba7f6
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-05-13 18:25:44 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -54,7 +54,7 @@ router.get('/mutlaa', function(req, res, next) {
   req.session.mutlaa = true;
   req.session.save(function(err) {
     // session saved
-    res.redirect('/mutlaaWelcome')
+    res.redirect('/mutlaa')
   })
 
 
