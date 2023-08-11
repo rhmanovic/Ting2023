f048d7007dc2d224d3991adb33ff964a947766aa
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-08-11 19:26:34 +0300

    Update category.js

--- models/category.js
+++ models/category.js
@@ -2,9 +2,33 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 var CategorySchema = new mongoose.Schema({
     categoryNo: Number,
+    categoryNo: String,
     img: String,
 
-    name: String
+    name: {
+        type: String,
+        default: ""
+    },
+    nameE: {
+        type: String,
+        default: ""
+    },
+    URLname: {
+        type: String,
+        default: ""
+    },
+    parent: {
+        type: String,
+        default: "/"
+    },
+    category: {
+        type: String,
+        default: "/"
+    },
+    grade: {
+        type: Number,
+        default: 0
+    } ,
 });
 
 var Category = mongoose.model('category', CategorySchema);
