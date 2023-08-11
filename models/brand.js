8783ba25fda3b5e89044e30416363d337b7b4bb2
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-08-11 19:25:14 +0300

    Update brand.js

--- models/brand.js
+++ models/brand.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 var BrandSchema = new mongoose.Schema({
     brandNo: Number,
-    name: String
+    name: String,
+    URLname: {
+      type: String,
+      default: ""
+    },
 });
 
 var Brand = mongoose.model('brand', BrandSchema);
