2b6d5c5459a804ac32e151c2fe2f92b23fc775e0
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-08-29 17:01:12 +0300

    Delete models directory

--- models/vendor.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
-var VendorSchema = new mongoose.Schema({
-    vendorNo: Number,
-    name: String
-});
-
-var Vendor = mongoose.model('vendor', VendorSchema);
-module.exports = Vendor;
