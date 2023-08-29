b0e79202d16341a461adf37c87dbdbbb2f30e793
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2023-08-29 16:27:43 +0300

    Delete models directory

--- models/warehouse.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
-var WarehouseSchema = new mongoose.Schema({
-    name: String
-});
-
-var Warehouse = mongoose.model('warehouse', WarehouseSchema);
-module.exports = Warehouse;
