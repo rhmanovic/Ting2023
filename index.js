f08966f59bbd4a911523425c3cfd4e26c980784e
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-02-04 21:19:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -154,6 +154,7 @@ app.use(function(err, req, res, next) {
 
 // Make language.json available in layout.pug
 global.language = require('./data/language.json');
+global.SiteImages = require('./data/SiteImages.json');
 
 
 
