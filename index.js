aa5ec1297fc4230d7560a35f0cf1a6476e4185a4
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-02-04 21:28:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,161 +1,139 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo');
-var mid = require('./middleware');
-// const passport = require('passport');
-// const passportSetup = require('./config/passport-setup');
-var bodyParser = require('body-parser');
-const multer = require('multer');
-const keys = require('./config/keys');
+doctype html(lang='en')
+head
 
-
-var routes = require('./routes/index');
-var privates = require('./privates/index');
-var routesAdmin = require('./routes/admin/index');
-var routesManager = require('./routes/manager/index');
-const authRoutes = require('./routes/auth-routes');
-const profileRoutes = require('./routes/profile-routes');
+  - var websiteLangaue = "ar"
+  
+  if choosedLangaue === "ar"
+    - websiteLangaue = "ar"
+  else if choosedLangaue === "en"
+    - websiteLangaue = "en"
 
 
 
-var app = express();
-
-// set view engine
-app.set('view engine', 'pug');
-
-// Bootstrap
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+  
+  if websiteLangaue === 'ar'
+    - var theLanguage = language.language.ar
+  else if websiteLangaue === 'en'
+    - var theLanguage = language.language.en
+    
+  // theLanguage.avaliable
 
 
-// initialize passport
-// app.use(passport.initialize());
-// app.use(passport.session());
+  
+
+  
 
 
+  // Google tag (gtag.js)
+  script(async='' src='https://www.googletagmanager.com/gtag/js?id=AW-731533762')
+  script.
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-731533762');
 
+  meta(charset='utf-8')
+  meta(name='viewport', content='width=device-width')
+  meta(http-equiv='x-ua-compatible', content='ie=edge')
+  meta(name='google-site-verification' content='27jonZI5RK1szTlfUxJ1yNgORCaqRR1kAXNIbSm56iI')
+  <link rel="preload" as="font" href="/fonts/Alexandria-Bold.ttf" type="font/ttf" crossorigin="anonymous">
+  <link rel="preload" as="font" href="/fonts/Alexandria-Regular.ttf" type="font/ttf" crossorigin="anonymous">
+  link(rel='icon' href='/img/upload/icon-t.jpg')
 
-
-// dublicted in app an admin
-const storage = multer.diskStorage({
-  destination: './privates/index/upLoads',
-  filename: function(req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-// init upload
-const upload = multer({
-  storage: storage
-}).single('myFile');
-
-
-// connect to mongodb ATLAS
-mongoose.set('strictQuery', false);
-mongoose.connect(keys.mongodb.dbURI, () => { console.log('connected to mongodb') });
-
-
-// mongoose.connect(keys.mongodb.dbURI)
-//   .then(() => console.log('connected to mongo'))
-//   .catch((err) => console.log('Connection failed'));
+  if productData
+    meta(property='og:image' content=productData.img[0])
+    meta(property='og:title' content=productData.name)
+    meta(property='og:description' content=`السعر: ${(productData.price).toFixed(3)} K.D`)
 
 
 
-var db = mongoose.connection;
+  title= theLanguage.title
+
+ 
+  link(href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD' crossorigin='anonymous')
 
 
 
+  link(href='https://fonts.googleapis.com/icon?family=Material+Icons', rel='stylesheet')
 
-// mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
+  link(rel='stylesheet', href='/stylesheets/style.css')
 
-app.use(session({
-  secret: 'We loves you',
-  resave: true,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: keys.mongodb.dbURI
-  }),
-})
-);
+  
+  // Meta Pixel Code
+  script.
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '213362121422781');
+    fbq('track', 'PageView');
+  noscript
+    img(height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id=213362121422781&ev=PageView&noscript=1')
+  // End Meta Pixel Code
 
+  meta(name='facebook-domain-verification' content='gwc8uhc1ybftvaop76hxv6nprxvaxs')
 
+  
+  
+  
 
+  
+  
 
-// make user ID available in templates
-app.use(function(req, res, next) {
-  res.locals.currentUser = req.session.userId;
-  res.locals.cartCountNow = req.session.cartCount;
-  res.locals.managerNow = req.session.manager;
-  res.locals.theSalesemanNow = req.session.theSaleseman;
-  res.locals.currentUserName = req.session.userName;
-  res.locals.theMutlaa = req.session.mutlaa;
-  res.locals.source = req.session.source;
-  res.locals.choosedLangaue = req.session.choosedLangaue;
+  
+  body.d-flex.flex-column.min-vh-100.Alexfontnormal(onload="theAjax();")
+    
+    include navbar
+    include includes/SearchNav   
+    
+    block content
 
-  next();
-});
+    include footer
+    
 
 
 
 
 
-// parse incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// serve static files from /public
-app.use(express.static(__dirname + '/public'));
-
-
-// include routes
-app.use('/', routes);
-app.use('/auth', authRoutes);
-app.use('/privates', privates);
-app.use('/admin', routesAdmin);
-app.use('/manager', routesManager);
-app.use('/profile', profileRoutes);
-app.set('views', __dirname + '/views');
-app.set('views/admin', __dirname + '/views/manager');
-app.set('views/manager', __dirname + '/views/manager');
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-// define as the last app.use callback
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-// listen on port 5000
-app.listen(3000, function() {
-  console.log('Express app listening on port 3000');
-});
-
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500)
-  // .json(response.error(err.status || 500));
-});
 
 
 
-// Make language.json available in layout.pug
-global.language = require('./data/language.json');
-global.SiteImages = require('./data/SiteImages.json');
 
 
 
-module.exports = app;
+
+
+
+
+
+
+
+    
+    
+    script(src="https://kit.fontawesome.com/f70269f5c4.js" crossorigin="anonymous")
+
+    script(src='https://code.jquery.com/jquery-3.5.1.slim.min.js', integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj', crossorigin='anonymous')
+    script(src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo', crossorigin='anonymous')
+
+    script(src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' integrity='sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN' crossorigin='anonymous')
+
+
+    script(src='/script-increment.js')
+    script.
+      $('#display').on('hidden.bs.modal', function (e) {
+        console.log('modal closed stop iframe');
+        document.getElementById("divIdVid").src = '';
+      })
+
+    script(src='/lazy-load.js')
+    script(src='/input-change.js')
+    script.
+      new LazyLoad();
+
+
+    script(src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js')
+    
