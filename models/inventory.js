55bcab0ca2465771b0ce5c5dd9edd698ba9dbbbf
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-02-14 23:19:15 +0300

    Update inventory.js

--- models/inventory.js
+++ models/inventory.js
@@ -56,6 +56,10 @@ var InventorySchema = new mongoose.Schema({
       type: Number,
       default: 0,
     },
+    minShop: {
+      type: Number,
+      default: 0,
+    },
     sellcount: {
       type: Number,
       default: 0,
