715aa6dcdc95c87a33fe9c697bb055ce4d1b754e
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-02-23 01:39:23 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -40,25 +40,25 @@ var UserSchema = new mongoose.Schema({
     googleId: String,
     // email: String
 });
-// // authenticate input against database documents
-UserSchema.statics.authenticate = function(email, password, callback) {
-  User.findOne({ email: email })
-      .exec(function (error, user) {
-        if (error) {
-          return callback(error);
-        } else if ( !user ) {
-          var err = new Error('User not found.');
-          err.status = 401;
-          return callback(err);
-        }
-        bcrypt.compare(password, user.password , function(error, result) {
-          if (result === true) {
-            return callback(null, user);
-          } else {
-            return callback();
-          }
-        })
-      });
+// authenticate input against database documents
+UserSchema.statics.authenticate = async function(email, password, callback) {
+  try {
+    const user = await User.findOne({ email: email }).exec();
+    if (!user) {
+      var err = new Error('User not found.');
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function(error, result) {
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
+    });
+  } catch (error) {
+    return callback(error);
+  }
 }
 // hash password before saving to database
 UserSchema.pre('save', function(next) {
