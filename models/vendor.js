4878ad08d69415da3892d79727f9de933c52f7df
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-02-26 16:59:40 +0300

    Update vendor.js

--- models/vendor.js
+++ models/vendor.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 var VendorSchema = new mongoose.Schema({
     vendorNo: Number,
-    name: String
+    name: String,
+    mobile: {
+      type: String,
+      default: ""
+    },
+    
+  vendorBrands: {
+    type: Array,
+    default: [],
+  }
 });
 
 var Vendor = mongoose.model('vendor', VendorSchema);
