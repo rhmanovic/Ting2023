4340984b9889e98f61c595fd57113a56fd65b94e
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-03-18 20:44:44 +0300

    Update index.js

--- routes/manager/index.js
+++ routes/manager/index.js
@@ -8,14 +8,12 @@ var Brand = require("../../models/brand");
 var Warehouse = require("../../models/warehouse");
 var Order = require("../../models/order");
 var Purchase = require("../../models/purchase");
-const CashFlow = require("../../models/cash");
 var TransferRequest = require("../../models/transferRequest");
 var User = require("../../models/user");
 var City = require("../../models/city");
 var mid = require("../../middleware");
 const keys = require("../../config/keys");
 const fs = require("fs");
-const jsQR = require('jsqr');
 
 const sharp = require('sharp');
 
