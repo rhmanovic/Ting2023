f74129d9fa387129fbae7be39fb4baaa9bb904f4
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-03-22 00:54:35 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -6,9 +6,9 @@ module.exports = {
     clientSecret: 'oVmsdLuFMI16dBL0ivNOMyf2'
   },
   mongodb: {
-    // dbURI: 'mongodb+srv://user02:Aa66787785@cluster0-hmsvk.gcp.mongodb.net/tingStore?retryWrites=true'
+    dbURI: 'mongodb+srv://user02:Aa66787785@cluster0-hmsvk.gcp.mongodb.net/tingStore?retryWrites=true'
     // dbURI: 'mongodb+srv://itcstore:Aa55559294@cluster0.k0kof.gcp.mongodb.net/?retryWrites=true&w=majority'
-    dbURI: 'mongodb+srv://user2023:123789@cluster0.k0kof.gcp.mongodb.net/?retryWrites=true&w=majority'
+    // dbURI: 'mongodb+srv://user2023:123789@cluster0.k0kof.gcp.mongodb.net/?retryWrites=true&w=majority'
   },
   admin: {
     Id: 'eng.dugaim@gmail.com'
